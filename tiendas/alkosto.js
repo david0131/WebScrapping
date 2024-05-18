1a6e12b87ea00e40e337be0dfbee82ba654164ac
david0131 <escorciaad@uninorte.edu.co>
2024-05-18 14:17:04 -0500

    Commit 6

--- tiendas/alkosto.js
+++ tiendas/alkosto.js
@@ -3,16 +3,13 @@ import { chromium } from 'playwright';
 export async function scrapeAlkosto(productName) {
     const browser = await chromium.launch({ headless: false });
     const page = await browser.newPage();
-    const productPrefix = productName.substring(0, 5);
 
     page.on('console', message => console.log('BROWSER-CONSOLE:', message.text()));
 
     console.log('Navegando a la página de Alkosto...');
     await page.goto('https://www.alkosto.com/', { waitUntil: 'domcontentloaded' });
     console.log('Rellenando el campo de búsqueda...');
-    await page.$eval('#js-site-search-input', (element, value) => {
-        element.value = value;
-    }, productName);
+    await page.fill('#js-site-search-input', productName);
     await page.evaluate(() => {
         document.querySelector('#js-search-button').disabled = false;
     });
@@ -22,23 +19,34 @@ export async function scrapeAlkosto(productName) {
     await page.waitForSelector('.ais-InfiniteHits-item', { timeout: 60000 });
 
     console.log('Extrayendo información de los productos...');
-    const products = await page.$$eval('.ais-InfiniteHits-item', (nodes, productPrefix) => {
-        // Extraer los productos y formatear la información
-        const productList = nodes.slice(0, 5).map(node => {
-            const title = node.querySelector('.product__item__top__title')?.innerText;
+    let products = await page.$$eval('.ais-InfiniteHits-item', (nodes, productName) => {
+        // Función para eliminar tildes
+        function removeAccents(str) {
+            return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+        }
+
+        const productNameNormalized = removeAccents(productName).toLowerCase().split(' ');
+
+        // Extraer y filtrar los productos
+        const productList = nodes.map(node => {
+            const title = node.querySelector('.product__item__top__title')?.innerText || '';
             let priceText = node.querySelector('.product__price--discounts__price .price')?.innerText || '';
             const price = parseFloat(priceText.replace(/[^\d,.-]+/g, '').replace(',', '.'));
             const formattedPrice = `${priceText}`;
-            const image = node.querySelector('.product__item__information__image img')?.src;
-            const link = node.querySelector('.product__item__information__view-details a')?.href;
-            return { title, priceText: formattedPrice, image, link };
+            const image = node.querySelector('.product__item__information__image img')?.src || '';
+            const link = node.querySelector('.product__item__information__view-details a')?.href || '';
+            return { title, priceText: formattedPrice, price, image, link };
+        }).filter(product => {
+            const productTitleNormalized = removeAccents(product.title).toLowerCase();
+            if (productTitleNormalized === productNameNormalized.join(' ')) {
+                return true; // Si el título coincide exactamente, incluir el producto
+            }
+            // Si no coincide exactamente, buscar todas las palabras de productNameNormalized en el título
+            return productNameNormalized.every(word => productTitleNormalized.includes(word));
         });
 
-        // Filtrar los productos que comienzan con el prefijo del producto proporcionado
-        return productList.filter(product =>
-            product.title.toLowerCase().startsWith(productPrefix.toLowerCase())
-        );
-    }, productPrefix);
+        return productList;
+    }, productName);
 
     // Ordenar los productos por precio y seleccionar los 3 más baratos
     const sortedProducts = products.sort((a, b) => a.price - b.price);
--- tiendas/exito.js
+++ tiendas/exito.js
@@ -3,7 +3,6 @@ import { chromium } from 'playwright';
 export async function scrapeExito(productName) {
     const browser = await chromium.launch({ headless: false });
     const page = await browser.newPage();
-    const productPrefix = productName.substring(0, 5);
 
     page.on('console', message => console.log('BROWSER-CONSOLE:', message.text()));
 
@@ -12,19 +11,26 @@ export async function scrapeExito(productName) {
 
     let products = [];
     let currentPage = 0;
-    const maxPages = 2; // Define el número máximo de páginas a las que quieres navegar
+    const maxPages = 5; // Define el número máximo de páginas a las que quieres navegar
 
     do {
         console.log(`Checking page ${currentPage + 1} for products...`);
         console.log('Filling in the search field...');
         await page.fill('input[data-testid="store-input"]', productName);
         console.log('Pressing Enter');
-        await page.keyboard.press('Enter')
+        await page.keyboard.press('Enter');
         console.log('Waiting for the search results to load...');
         await page.waitForSelector('article[data-testid="store-product-card"]', { timeout: 60000 });
 
         console.log('Extracting product information...');
-        let newProducts = await page.$$eval('article[data-testid="store-product-card"]', (nodes, productPrefix) => {
+        let newProducts = await page.$$eval('article[data-testid="store-product-card"]', (nodes, productName) => {
+            // Función para eliminar tildes
+            function removeAccents(str) {
+                return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+            }
+
+            const productNameNormalized = removeAccents(productName).toLowerCase().split(' ');
+
             const productList = nodes.map(node => {
                 const titleElement = node.querySelector('h3[data-fs-product-card-title="true"] a');
                 if (!titleElement) return null; // Si no se encuentra el elemento del título, retorna null
@@ -35,20 +41,32 @@ export async function scrapeExito(productName) {
                 let priceText = node.querySelector('p[data-fs-container-price-otros="true"]')?.innerText || '';
                 const price = parseFloat(priceText.replace(/[^\d,.-]+/g, '').replace(',', '.')) || 0;
                 const formattedPrice = `${priceText}`;
-                const image = node.querySelector('a[data-fs-link="true"] img')?.src;
-                const link = node.querySelector('h3[data-fs-product-card-title="true"] a')?.href;
-                return { title, priceText: formattedPrice, image, link };
+                const image = node.querySelector('a[data-fs-link="true"] img')?.src || '';
+                const link = titleElement.href;
+                return { title, priceText: formattedPrice, price, image, link };
             }).filter(Boolean); // Eliminar nulos
 
-            return productList.filter(product =>
-                product.title.toLowerCase().startsWith(productPrefix.toLowerCase())
-            );
-        }, productPrefix);
+            let exactMatchProducts = productList.filter(product => {
+                const productTitleNormalized = removeAccents(product.title).toLowerCase();
+                return productTitleNormalized.includes(productNameNormalized.join(' '));
+            });
+            
+            // Luego, buscar productos que contengan todas las palabras de productNameNormalized pero no necesariamente en ese orden
+            let partialMatchProducts = productList.filter(product => {
+                const productTitleNormalized = removeAccents(product.title).toLowerCase();
+                const containsAllWords = productNameNormalized.every(word => productTitleNormalized.includes(word));
+                const excludesTerms = !['cargador', 'fundas'].some(term => productTitleNormalized.includes(term));
+                return containsAllWords && excludesTerms;
+            });
+            
+            // Combinar los productos encontrados
+            return [...exactMatchProducts, ...partialMatchProducts];
+        }, productName);
 
         products = [...products, ...newProducts];
 
-        if (products.length >= 3 || currentPage >= maxPages - 1) {
-            break; // Si ya tienes 3 productos o has alcanzado el máximo de páginas, detente
+        if (products.length >= 5 || currentPage >= maxPages - 1) {
+            break; // Si ya tienes 5 productos o has alcanzado el máximo de páginas, detente
         }
 
         console.log('Not enough products found, going to next page...');
@@ -63,9 +81,16 @@ export async function scrapeExito(productName) {
         }
     } while (currentPage < maxPages);
 
+    // Si hay más de 5 productos, solo tomamos los primeros 5
+    if (products.length > 5) {
+        products = products.slice(0, 5);
+    }
+
+    // Ordenar los productos por precio y seleccionar los 3 más baratos
     const sortedProducts = products.sort((a, b) => a.price - b.price);
     const cheapestProducts = sortedProducts.slice(0, 3);
 
+    // Mostrar precios por consola
     cheapestProducts.forEach(product => {
         console.log(`Product: ${product.title}, Price: ${product.priceText}`);
     });
